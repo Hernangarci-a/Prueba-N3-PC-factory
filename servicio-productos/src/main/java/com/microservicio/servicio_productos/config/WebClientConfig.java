fb5ab609ced2b62af3eb4232a47c16ce2e53bf8b
Hernan G <hernanignaciosanchez@gmail.com>
2026-06-13 16:52:03 -0400

    se agregra la carpeta y clase web client

--- servicio-productos/src/main/java/com/microservicio/servicio_productos/config/WebClientConfig.java
+++ servicio-productos/src/main/java/com/microservicio/servicio_productos/config/WebClientConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient.Builder webClientBuilder() {
+    public WebClient.Builder webClientBuilder() { // web client
         return WebClient.builder();
     }
+
 }
